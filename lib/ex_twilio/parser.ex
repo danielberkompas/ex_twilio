d1c5cf776d82497ce28e4ebf3f3534020eb92c1c
Daniel Berkompas <himself@danielberkompas.com>
2015-03-26 07:07:53 -0700

    Add parser tests

--- lib/ex_twilio/parser.ex
+++ lib/ex_twilio/parser.ex
@@ -1,10 +1,21 @@
 defmodule ExTwilio.Parser do
+  @type metadata         :: map
+  @type http_status_code :: number
+  @type key              :: String.t
+  @type response         :: %{body: String.t, status_code: number}
+  @type success          :: {:ok, [map]}
+  @type success_list     :: {:ok, [map], metadata}
+  @type success_delete   :: :ok
+  @type error            :: {:error, String.t, http_status_code}
+
+  @spec parse(atom, response) :: success | error
   def parse(module, response) do
     handle_errors response, fn(body) ->
       Poison.decode!(body, as: module)
     end
   end
 
+  @spec parse_list(atom, response, key) :: success_list | error
   def parse_list(module, response, key) do
     result = handle_errors response, fn(body) ->
       as = Dict.put(%{}, key, [module])
@@ -17,7 +28,8 @@ defmodule ExTwilio.Parser do
     end
   end
 
-  def handle_errors(response, fun) do
+  @spec handle_errors(response, ((String.t) -> any)) :: success | success_delete | error
+  defp handle_errors(response, fun) do
     case response do
       %{body: body, status_code: status} when status in [200, 201] -> 
         {:ok, fun.(body)}
---
+++ test/ex_twilio/parser_test.exs
@@ -0,0 +1,41 @@
+defmodule ExTwilio.ParserTest do
+  use ExUnit.Case
+  import ExTwilio.Parser
+
+  defmodule Resource do
+    defstruct sid: nil
+  end
+
+  test ".parse should decode a successful response into a named struct" do
+    response = %{body: "{ \"sid\": \"unique_id\" }", status_code: 200}
+    assert {:ok, %Resource{sid: "unique_id"}} == parse(Resource, response)
+  end
+
+  test ".parse should return an error when response is 400" do
+    response = %{body: "{ \"message\": \"Error message\" }", status_code: 400}
+    assert {:error, "Error message", 400} == parse(Resource, response)
+  end
+
+  test ".parse should return :ok when response is 204 'No Content'" do
+    response = %{body: "", status_code: 204}
+    assert :ok == parse(Resource, response)
+  end
+  
+  test ".parse_list should decode into a list of named structs" do
+    json = """
+    {
+      "resources": [{
+        "sid": "first"
+      }, {
+        "sid": "second"
+      }],
+      "next_page": 10
+    }
+    """
+    response = %{body: json, status_code: 200}
+    expected = [%Resource{sid: "first"}, %Resource{sid: "second"}]
+    metadata = %{"next_page" => 10}
+
+    assert {:ok, expected, metadata} == parse_list(Resource, response, "resources")
+  end
+end
