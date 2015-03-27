26d865258a500572c71202e4e10e68a03429b1d7
Daniel Berkompas <himself@danielberkompas.com>
2015-03-27 06:50:23 -0700

    Add support for Accounts

---
+++ lib/ex_twilio/resources/account.ex
@@ -0,0 +1,62 @@
+defmodule ExTwilio.Account do
+  @moduledoc """
+  Represents an Account or Subaccount resource.
+
+  - [Account docs](https://www.twilio.com/docs/api/rest/accounts)
+  - [Subaccount docs](https://www.twilio.com/docs/api/rest/subaccounts)
+  """
+
+  use ExTwilio.Resource, import: [:stream, :all, :list, :find, :create, :update]
+
+  defstruct sid: nil,
+            owner_account_sid: nil,
+            date_created: nil,
+            date_updated: nil,
+            friendly_name: nil,
+            type: nil,
+            status: nil,
+            auth_token: nil,
+            uri: nil,
+            subresource_uris: nil
+
+  @doc """
+  Suspend an Account by updating its status to "suspended".
+
+  - [Twilio Docs](https://www.twilio.com/docs/api/rest/subaccounts#suspending-subaccounts)
+
+  ## Example
+
+      {:ok, account} = ExTwilio.Account.find("<sid>")
+      ExTwilio.Account.suspend(account)
+  """
+  @spec suspend(map | String.t) :: Parser.success | Parser.error
+  def suspend(%{sid: sid}), do: suspend(sid)
+  def suspend(sid),         do: update(sid, status: "suspended")
+
+  @doc """
+  Reactivate a suspended Account by updating its status to "active".
+
+  - [Twilio Docs](https://www.twilio.com/docs/api/rest/subaccounts#suspending-subaccounts)
+
+  ## Example
+
+      {:ok, account} = ExTwilio.Account.find("<sid>")
+      ExTwilio.Account.reactivate(account)
+  """
+  def reactivate(%{sid: sid}), do: reactivate(sid)
+  def reactivate(sid),         do: update(sid, status: "active")
+
+  @doc """
+  Permanently close an Account by updating its status to "closed". This cannot
+  be undone, so use it carefully!
+
+  - [Twilio Docs](https://www.twilio.com/docs/api/rest/subaccounts#closing-subaccounts)
+
+  ## Example
+
+      {:ok, account} = ExTwilio.Account.find("<sid>")
+      ExTwilio.Account.close(account)
+  """
+  def close(%{sid: sid}), do: close(sid)
+  def close(sid),         do: update(sid, status: "closed")
+end
