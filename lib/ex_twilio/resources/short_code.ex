ba257e5c624805cd7e9e63783cb96108ed6ee790
Daniel Berkompas <himself@danielberkompas.com>
2015-04-10 16:23:53 -0700

    [#9] Support all Twilio API endpoints

---
+++ lib/ex_twilio/resources/short_code.ex
@@ -0,0 +1,25 @@
+defmodule ExTwilio.ShortCode do
+  @moduledoc """
+  Represents a ShortCode resource in the Twilio API.
+   
+  - [Twilio docs](https://www.twilio.com/docs/api/rest/short-codes)
+  """
+
+  use ExTwilio.Resource, import: [:stream, :all, :list, :find, :update]
+
+  defstruct sid: nil,
+            date_created: nil,
+            date_updated: nil,
+            friendly_name: nil,
+            account_sid: nil,
+            short_code: nil,
+            api_version: nil,
+            sms_url: nil,
+            sms_method: nil,
+            sms_fallback_url: nil,
+            sms_fallback_url_method: nil,
+            uri: nil
+
+  def resource_name, do: "SMS/ShortCodes"
+  def parents, do: [:account]
+end
---
+++ lib/ex_twilio/resources/sip_domain.ex
@@ -0,0 +1,29 @@
+defmodule ExTwilio.SipDomain do
+  @moduledoc """
+  Represents a SIP Domain resource in the Twilio API.
+
+  - [Twilio docs](https://www.twilio.com/docs/api/rest/domain)
+  """
+
+  use ExTwilio.Resource, import: [:stream, :all, :list, :find, :create, :update, :destroy]
+
+  defstruct sid: nil,
+            friendly_name: nil,
+            account_sid: nil,
+            api_version: nil,
+            domain_name: nil,
+            auth_type: nil,
+            voice_url: nil,
+            voice_method: nil,
+            voice_fallback_url: nil,
+            voice_fallback_method: nil,
+            voice_status_callback_url: nil,
+            voice_status_callback_method: nil,
+            date_created: nil,
+            date_updated: nil,
+            uri: nil
+
+  def resource_name, do: "SIP/Domains"
+  def resource_collection_name, do: "domains"
+  def parents, do: [:account]
+end
