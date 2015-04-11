bd0353415f3f09febcfb9b9b4ddbad8d239f6f4e
Daniel Berkompas <himself@danielberkompas.com>
2015-04-11 10:20:38 -0700

    Fill out struct for DependentPhoneNumber

--- lib/ex_twilio/resources/dependent_phone_number.ex
+++ lib/ex_twilio/resources/dependent_phone_number.ex
@@ -5,7 +5,15 @@ defmodule ExTwilio.DependentPhoneNumber do
   - [Twilio docs](https://www.twilio.com/docs/api/rest/dependent-phone-numbers)
   """
 
-  defstruct sid: nil
+  defstruct sid: nil,
+            account_sid: nil,
+            friendly_name: nil,
+            customer_name: nil,
+            street: nil,
+            city: nil,
+            region: nil,
+            postal_code: nil,
+            iso_country: nil
 
   use ExTwilio.Resource, import: [:stream, :all, :list]
 
