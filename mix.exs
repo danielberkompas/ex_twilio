8a5eabe301334322b36ebf57afa751f07f40a8f5
Daniel Berkompas <himself@danielberkompas.com>
2016-06-06 07:45:37 -0700

    Bump version to 0.1.8

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExTwilio.Mixfile do
 
   def project do
     [app: :ex_twilio,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.0",
      name: "ExTwilio",
      description: "Twilio API library for Elixir",
