4c7d0f9612d82487b077be4085f2a549e82157e8
Erik Nilsen <enilsen16@live.com>
2016-11-09 18:29:29 -0800

    add worker capability token

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
-config :ex_twilio, account_sid: {:system, "TWILIO_TEST_ACCOUNT_SID"},
-                   auth_token:  {:system, "TWILIO_TEST_AUTH_TOKEN"}
+config :ex_twilio, account_sid: System.get_env("TWILIO_TEST_ACCOUNT_SID"),
+                   auth_token:  System.get_env("TWILIO_TEST_AUTH_TOKEN"),
+                   workspace_sid: System.get_env("TWILIO_TEST_WORKSPACE_SID")
 config :logger,
   level: :info
