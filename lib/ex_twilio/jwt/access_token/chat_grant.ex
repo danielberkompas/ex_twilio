852794886da3ff14e485b0399f574959df3988bb
Daniel Berkompas <himself@danielberkompas.com>
2018-04-16 12:22:23 -0700

    Improve documentation, run travis on later Elixir

--- lib/ex_twilio/jwt/access_token/chat_grant.ex
+++ lib/ex_twilio/jwt/access_token/chat_grant.ex
@@ -1,7 +1,30 @@
 defmodule ExTwilio.JWT.AccessToken.ChatGrant do
+  @moduledoc """
+  A JWT grant to access a given Twilio chat service.
+
+      ExTwilio.JWT.AccessToken.ChatGrant.new(
+        service_sid: "sid",
+        endpoint_id: "123",
+        deployment_role_sid: "sid",
+        push_credential_sid: "sid"
+      )
+  """
+
   @enforce_keys [:service_sid]
+
   defstruct service_sid: nil, endpoint_id: nil, deployment_role_sid: nil, push_credential_sid: nil
 
+  @type t :: %__MODULE__{
+          service_sid: String.t(),
+          endpoint_id: String.t(),
+          deployment_role_sid: String.t(),
+          push_credential_sid: String.t()
+        }
+
+  @doc """
+  Create a new grant.
+  """
+  @spec new(attrs :: Keyword.t()) :: t
   def new(attrs \\ []) do
     struct(__MODULE__, attrs)
   end
