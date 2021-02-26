3a221d5954a34ce9091a81446b8f5878efdee422
Ryan Stimmler <ryan@workpath.co>
2021-02-26 11:40:40 -0700

    fix configs, fix env sample, update httpoison

--- config/dev.exs
+++ config/dev.exs
@@ -4,4 +4,4 @@ config :ex_twilio,
   account_sid: {:system, "TWILIO_ACCOUNT_SID"},
   auth_token: {:system, "TWILIO_AUTH_TOKEN"},
   workspace_sid: {:system, "TWILIO_WORKSPACE_SID"},
-  proxy_service_sid: {:system "TWILIO_PROXY_SERVICE_SID"}
+  proxy_service_sid: {:system, "TWILIO_PROXY_SERVICE_SID"}
--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,6 @@ config :ex_twilio,
   account_sid: {:system, "TWILIO_TEST_ACCOUNT_SID"},
   auth_token: {:system, "TWILIO_TEST_AUTH_TOKEN"},
   workspace_sid: {:system, "TWILIO_WORKSPACE_SID"},
-  proxy_service_sid: {:system "TWILIO_PROXY_SERVICE_SID"}
+  proxy_service_sid: {:system, "TWILIO_PROXY_SERVICE_SID"}
 
 config :logger, level: :info
